82b913d456900c0c0ae45302c7dd569d54d064a6
Ricard <r.molina91@gmail.com>
2018-08-21 09:31:29 +0200

    Adding validations to usuario schema fields

--- models/usuario.js
+++ models/usuario.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 let Schema = mongoose.Schema;
+
+let roles = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+}
 
 let usuarioSchema = new Schema({
     nombre:{
@@ -8,6 +14,7 @@ let usuarioSchema = new Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: [true, 'El correo es necesario']
     },
     password: {
@@ -19,7 +26,8 @@ let usuarioSchema = new Schema({
     },
     role: {
         type: String,
-        default: 'USER_ROLE'
+        default: 'USER_ROLE',
+        enum: roles
     }, 
     estado: {
         type: Boolean,
@@ -31,5 +39,7 @@ let usuarioSchema = new Schema({
     }
 
 });
+
+usuarioSchema.plugin(uniqueValidator, {message: '{PATH} debe de ser unico'})
 
 module.exports = mongoose.model('usuario', usuarioSchema);

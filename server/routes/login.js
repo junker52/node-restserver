158690d0fc43a79ef4000e74d2489ae945fc51fd
Ricard <r.molina91@gmail.com>
2018-08-22 11:26:30 +0200

    Added normal POST login

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,8 +5,37 @@ const _ = require('underscore');
 const Usuario = require('../../models/usuario');
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
+    let body = req.body;
+    Usuario.findOne({email: body.email}, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            }); 
+        }
+        if (!usuarioDB ) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El usuario no existe'
+                }
+            })
+        }
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El password no es correcto'
+                }
+            })
+        }
+
+        return res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token: '123'
+        })
+        
     })
 })
 
